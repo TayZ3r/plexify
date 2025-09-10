928e0d761939dcbdd7aff7851ecbb25f6cb88cb0
Quentin W. <20359522+DotDebian@users.noreply.github.com>
2025-09-11 01:52:31 +0200

    Conditional assignment for profileId and rootFolder

--- server/entity/MediaRequest.ts
+++ server/entity/MediaRequest.ts
@@ -329,8 +329,8 @@ export class MediaRequest {
           : undefined,
         is4k: requestBody.is4k,
         serverId: requestBody.serverId,
-        profileId: requestBody.profileId,
-        rootFolder: requestBody.rootFolder,
+        profileId: tmdbMediaShow.keywords.some((keyword) => keyword.id === 210024) ? 7 : requestBody.profileId,
+        rootFolder: tmdbMediaShow.keywords.some((keyword) => keyword.id === 210024) ? "/downloads/Animes" : requestBody.rootFolder,
         languageProfileId: requestBody.languageProfileId,
         tags: requestBody.tags,
         seasons: finalSeasons.map(
